a7072490a30af17daacbf131e0b3e7e91d0e0617
Tom <2508893587@qq.com>
2026-01-24 02:56:53 +0800

    Quartz sync: Jan 24, 2026, 2:56 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "blog-quartz.vercel.app", // quartz.jzhao.xyz
     ignorePatterns: ["private", "templates", ".obsidian", "Excalidraw"], //忽略文件夹
     defaultDateType: "modified",
     theme: {
