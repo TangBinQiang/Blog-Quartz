577a0fb163bb3008a1c68b7355eb0d60e6e516bc
Tom <2508893587@qq.com>
2026-01-24 03:06:21 +0800

    Quartz sync: Jan 24, 2026, 3:06 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "blog-quartz.vercel.app", // quartz.jzhao.xyz
+    baseUrl: "blog-quartz.vercel.app", // 部署到 Vercel 时，请修改为自己的域名 如 blog-quartz.jzhao.xyz
     ignorePatterns: ["private", "templates", ".obsidian", "Excalidraw"], //忽略文件夹
     defaultDateType: "modified",
     theme: {
